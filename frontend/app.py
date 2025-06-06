41e7d74c9afc6df765ddffa29aa2b546cb8946e6
samsaminfohub <hachicha_sami@hotmail.fr>
2025-06-05 22:55:17 -0400

    Update app.py
    sd

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://backend:8000"
+API_URL = "http://20.62.91.133:8000"
 
 st.title("Gestionnaire de tâches")
 if st.button("Afficher les tâches"):
