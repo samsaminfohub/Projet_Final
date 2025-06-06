67ce26a469d4dcbbb64e3cfb7e140633256af420
samsaminfohub <hachicha_sami@hotmail.fr>
2025-06-05 23:00:11 -0400

    Update models.py
    sds

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean
 import database
 
-class Task(Base):
+class Task(database):
     __tablename__ = "tasks"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
